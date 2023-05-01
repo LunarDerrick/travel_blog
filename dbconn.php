effce33301f655897f06b0a866664a9c71afd6a4
NFSL2001 <s2116593@siswa.um.edu.my>
2023-05-01 14:20:26 +0800

    basic database connection php

---
+++ dbconn.php
@@ -0,0 +1,19 @@
+<?php
+
+// Basic connection settings
+$databaseHost = 'localhost';
+$databaseUsername = 'webdev';
+$databasePassword = 'HKAkU8bowx.41@R_';
+$databaseName = 'traveldb';
+
+// Connect to the database
+$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+// Check connection
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error);
+}
+
+
+// Sample code for access, delete before run
+$result = $conn -> query("SELECT * FROM users");
+var_dump($result -> fetch_assoc());
