69cce329754f1f9e6900a49bdbeb1cf569d9ee00
NFSL2001 <s2116593@siswa.um.edu.my>
2023-05-20 17:17:11 +0800

    404 page with php and htaccess (for apache)

--- init_check_logged_in.php
+++ init_check_logged_in.php
@@ -2,6 +2,6 @@
 
 if (!isset($_SESSION["username"])) {
     http_response_code(404);
-    //include('my_404.html'); // provide your own HTML for the error page
+    include('404.php'); // provide your own HTML for the error page
     die();
 }
