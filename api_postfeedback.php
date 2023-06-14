554c29f3bc6eed4ebc07d608b4f4cf2e659303ed
NFSL2001 <s2116593@siswa.um.edu.my>
2023-06-14 23:17:00 +0800

    add comments

--- api_postfeedback.php
+++ api_postfeedback.php
@@ -6,6 +6,8 @@ require_once "init_session.php";
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     if ( isset($_POST['setrating']) && isset($_POST["postid"]) ){
         return setRating($conn, $_POST['setrating'], $_POST['postid']);
+    } else if ( isset($_POST['comment']) && isset($_POST["postid"]) ){
+        return addComment($conn, $_POST['comment'], $_POST['postid']);
     } else {
         http_response_code(404);
         include('404.php'); // provide your own HTML for the error page
@@ -33,6 +35,30 @@ function setRating($conn, $rating, $postid) {
     }
     $stmt = $conn->prepare("REPLACE INTO ratings (userid, postid, rating) VALUES (?,?,?)");
     $stmt->bind_param("iss", $_SESSION['userid'], $postid, $rating);
+    if (!$stmt->execute()){
+        http_response_code(500);
+        die;
+    }
+    return JSONresponse(200, []);
+}
+
+
+function addComment($conn, $comment, $postid) {
+    // make sure both are number
+    if (
+        !filter_var($postid, FILTER_VALIDATE_INT)
+    ){
+        http_response_code(404);
+        include('404.php'); // provide your own HTML for the error page
+        die;
+    }
+    if (empty($_SESSION['userid'])){
+        return JSONresponse(401, ["message" => "Please login to comment."]);
+    }
+    $currenttime = round(microtime(TRUE) * 1000); // epoch timestamp
+
+    $stmt = $conn->prepare("INSERT INTO comments (userid, postid, commenttime, comment) VALUES (?,?,?,?)");
+    $stmt->bind_param("isis", $_SESSION['userid'], $postid, $currenttime, $comment);
     if (!$stmt->execute()){
         http_response_code(500);
         die;
