00af7b5980d8e62fbe90d35809e9c9fa466ee5d2
NFSL2001 <s2116593@siswa.um.edu.my>
2023-06-16 12:08:37 +0800

    refactor username function location

--- api_userinfo.php
+++ api_userinfo.php
@@ -20,3 +20,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     include('404.php'); // provide your own HTML for the error page
     die();
 }
+
+
+/**
+ * Find user in database and return user ID and password if found, or null if not. Only for internal use, do not expose to outside
+ * @param mysqli_connection $conn database connection
+ * @param string $username database connection
+ * @return array of {userid=>"", password=>""} if found username, or null if not found
+ */
+function verifyUsername($conn, $username){
+    $stmt = $conn->prepare("SELECT userid, password FROM users WHERE username=?");
+    $stmt->bind_param("s", $username);
+    if (!$stmt->execute()){
+        return null;
+    }
+    $result = $stmt->get_result();
+    if ($row = $result->fetch_assoc()){
+        return $row;
+    } else {
+        return null;
+    }
+}
--- init_db.php
+++ init_db.php
@@ -22,25 +22,6 @@ if ($conn -> connect_error) {
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
 
-/**
- * Find user in database and return user ID and password if found, or null if not. Only for internal use, do not expose to outside
- * @param mysqli_connection $conn database connection
- * @param string $username database connection
- * @return array of {userid=>"", password=>""} if found username, or null if not found
- */
-function verifyUsername($conn, $username){
-    $stmt = $conn->prepare("SELECT userid, password FROM users WHERE username=?");
-    $stmt->bind_param("s", $username);
-    if (!$stmt->execute()){
-        return null;
-    }
-    $result = $stmt->get_result();
-    if ($row = $result->fetch_assoc()){
-        return $row;
-    } else {
-        return null;
-    }
-}
 
 /**
  * Return JSON response given a variable
